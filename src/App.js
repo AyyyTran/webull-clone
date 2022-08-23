343f1859805e1cf5ff6a3d82ff408d8736e357aa
Andy Tran <AyyyTran@gmail.com>
2022-08-23 15:10:17 -0700

    initializing repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
