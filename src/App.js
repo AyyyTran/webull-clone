0915c5bc52c87e974431b27f1283bdb02bc7e6ad
Andy Tran <AyyyTran@gmail.com>
2022-08-23 16:14:56 -0700

    final code desktop version only

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Navbar />
       <Hero />
-    </div>
+    </>
   );
 }
 
